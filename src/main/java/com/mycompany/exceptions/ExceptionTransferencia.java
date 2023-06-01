9966de06b096685f667db46a70528fc24a045698
Igor Kaique <igor.k.pereira@hotmail.com>
2023-06-01 18:58:13 -0300

    Novas funções

--- src/main/java/com/mycompany/exceptions/ExceptionTransferencia.java
+++ src/main/java/com/mycompany/exceptions/ExceptionTransferencia.java
@@ -1,4 +1,4 @@
-package com.mycompany;
+package com.mycompany.exceptions;
 
 public class ExceptionTransferencia extends Exception{
 
--- src/main/java/com/mycompany/model/ModelBanco.java
+++ src/main/java/com/mycompany/model/ModelBanco.java
@@ -33,19 +33,53 @@ public class ModelBanco {
     }
 
     public void ordenarContasSaldo(){
+        for (int i = 0; i < numeroDeContas; i++){
+            for (int j = 0; j < numeroDeContas - 1; j++){
+                if (contas[j].getContaSaldo() > contas[j+1].getContaSaldo()){
+                    ModelContaBancaria temp = contas[j];
+                    contas[j] = contas[j+1];
+                    contas[j+1] = temp;
+                }
+            }
+        }
+    }
+
+    public void depositar(int numeroConta, double valor){
+        for (int i = 0; i < numeroDeContas; i++){
+            if (contas[i].getContaNumero() == numeroConta){
+                contas[i].deposito(valor);
+            }
+        }
+    }
+
+    public void sacar(int numeroConta, double valor){
+        ordenarContasSaldo();
+        int contaBuscada = buscaBinaria(numeroConta);
+        if (contaBuscada >= 0 ){
+            contas[contaBuscada].sacar(valor);
+            System.out.println("Obrigado "+contas[contaBuscada].getContaTitular()+
+                    ". Saque no valor de R$"+valor+" realizado com sucesso!");
+        } else {
+            System.out.println("Conta inexistente! Tente novamente.");
+        }
 
     }
 
-    public void depositar(){
+    private int buscaBinaria(int numeroConta){
+        int inicio = 0;
+        int fim = numeroDeContas - 1;
 
-    }
-
-    public void sacar(){
-
-    }
-
-    private int buscaBinaria(int NumeroConta){
-        return numeroConta;
+        while(inicio > fim){
+            int meio = (inicio + fim) / 2;
+            if (contas[meio].getContaNumero() == numeroConta){
+                return meio;
+            } else if (contas[meio].getContaNumero() < numeroConta){
+                inicio = meio + 1;
+            } else {
+                fim = meio -1;
+            }
+        }
+        return - 1;
     }
 
     private double calcularSaldoTotal(){
